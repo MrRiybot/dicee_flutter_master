7fc66fcdc46957cac25bcc1b106a7620b9b34b3f
MrRiybot <mrriybot@gmail.com>
2022-12-13 19:34:24 +0300

    'ending_project'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +15,43 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
-class DicePage extends StatelessWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  void randomizeDice(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: TextButton(
+                      onPressed: () {randomizeDice();},
+                      child: Image.asset('images/dice$leftDiceNumber.png')))),
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: TextButton(
+                      onPressed: () {randomizeDice();},
+                      child: Image.asset('images/dice$rightDiceNumber.png')))),
+
+        ],
+      ),
+    );
   }
 }
+
